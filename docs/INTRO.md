cbc78469b2fa70bbf5b9499a9968bb6853b10b9b
Hari <harisankar995@gmail.com>
2024-01-27 22:47:45 +0100

    docs updated, new example added for theta star

--- docs/INTRO.md
+++ docs/INTRO.md
@@ -1,5 +1,8 @@
 # Pathfinding3D
 
+[![MIT License](https://img.shields.io/github/license/harisankar95/pathfinding3d)](https://github.com/harisankar95/pathfinding3D/blob/main/LICENSE)
+[![PyPI](https://img.shields.io/pypi/v/pathfinding3d)](https://pypi.org/project/pathfinding3d/)
+
 ## Introduction
 
 Pathfinding algorithms for python3 froked from [python-pathfinding](https://github.com/brean/python-pathfinding) by [@brean](https://github.com/brean).
@@ -15,5 +18,7 @@ Currently there are 7 path-finders bundled in this library, namely:
 - Breadth First Search (BFS)
 - Iterative Deeping A\* (IDA\*): Memory efficient algorithm for large graphs.
 - Minimum Spanning Tree (MSP)
+- Theta\*: Almost A\* with path smoothing.
 
 Dijkstra, A\* and Bi-directional A\* take the weight of the fields on the map into account.
+Theta\* is a variant of A\* but with any angle of movement allowed.
---
+++ examples/04_theta_star.py
@@ -0,0 +1,163 @@
+"""
+This example shows how Theta* algorithm differs from A* algorithm
+Requires plotly for visualization. Install it using `pip install plotly`
+"""
+
+import numpy as np
+import plotly.graph_objects as go
+
+from pathfinding3d.core.diagonal_movement import DiagonalMovement
+from pathfinding3d.core.grid import Grid
+from pathfinding3d.finder.a_star import AStarFinder
+from pathfinding3d.finder.theta_star import ThetaStarFinder
+
+# Create a 3D numpy array with 0s as obstacles and 1s as walkable paths
+matrix = np.ones((10, 10, 10), dtype=np.int8)
+# mark the center of the grid as an obstacle
+matrix[5, 5, 5] = 0
+
+# Create a grid object from the numpy array
+grid = Grid(matrix=matrix)
+
+# Mark the start and end points
+start = grid.node(0, 0, 0)
+end = grid.node(9, 9, 9)
+
+# Create an instance of the Theta* finder with diagonal movement allowed
+finder = ThetaStarFinder(diagonal_movement=DiagonalMovement.always)
+path, runs = finder.find_path(start, end, grid)
+
+# Path will be a list with all the waypoints as nodes
+# Convert it to a list of coordinate tuples
+path = [p.identifier for p in path]
+
+print("operations:", runs, "path length:", len(path))
+print("path:", path)
+
+# clean up the grid
+grid.cleanup()
+
+# Create an instance of the A* finder with diagonal movement allowed
+finder = AStarFinder(diagonal_movement=DiagonalMovement.always)
+astar_path, runs = finder.find_path(start, end, grid)
+
+astar_path = [p.identifier for p in astar_path]
+
+print("AStarFinder operations:", runs, "AStarFinder path length:", len(astar_path))
+print("AStarFinder path:", astar_path)
+
+
+def calculate_path_cost(path):
+    cost = 0
+    for pt, pt_next in zip(path[:-1], path[1:]):
+        dx, dy, dz = pt_next[0] - pt[0], pt_next[1] - pt[1], pt_next[2] - pt[2]
+        cost += (dx**2 + dy**2 + dz**2) ** 0.5
+    return cost
+
+
+theta_star_cost = calculate_path_cost(path)
+astar_cost = calculate_path_cost(astar_path)
+
+print("ThetaStarFinder path cost:", theta_star_cost, "\nAStarFinder path cost:", astar_cost)
+
+# Create a plotly figure to visualize the path
+fig = go.Figure(
+    data=[
+        go.Scatter3d(
+            x=[pt[0] + 0.5 for pt in path],
+            y=[pt[1] + 0.5 for pt in path],
+            z=[pt[2] + 0.5 for pt in path],
+            mode="lines + markers",
+            line=dict(color="blue", width=4),
+            marker=dict(size=4, color="blue"),
+            name="Theta* path",
+            hovertext=["Theta* path point"] * len(path),
+        ),
+        go.Scatter3d(
+            x=[pt[0] + 0.5 for pt in astar_path],
+            y=[pt[1] + 0.5 for pt in astar_path],
+            z=[pt[2] + 0.5 for pt in astar_path],
+            mode="lines + markers",
+            line=dict(color="red", width=4),
+            marker=dict(size=4, color="red"),
+            name="A* path",
+            hovertext=["A* path point"] * len(astar_path),
+        ),
+        go.Scatter3d(
+            x=[5.5],
+            y=[5.5],
+            z=[5.5],
+            mode="markers",
+            marker=dict(color="black", size=7.5),
+            name="Obstacle",
+            hovertext=["Obstacle point"],
+        ),
+        go.Scatter3d(
+            x=[0.5],
+            y=[0.5],
+            z=[0.5],
+            mode="markers",
+            marker=dict(color="green", size=7.5),
+            name="Start",
+            hovertext=["Start point"],
+        ),
+        go.Scatter3d(
+            x=[9.5],
+            y=[9.5],
+            z=[9.5],
+            mode="markers",
+            marker=dict(color="orange", size=7.5),
+            name="End",
+            hovertext=["End point"],
+        ),
+    ]
+)
+
+# Define the camera position
+camera = {
+    "up": {"x": 0, "y": 0, "z": 1},
+    "center": {"x": 0.1479269806756467, "y": 0.06501594452841505, "z": -0.0907033779622012},
+    "eye": {"x": 1.3097359159706334, "y": 0.4710974884501846, "z": 2.095154166796815},
+    "projection": {"type": "perspective"},
+}
+
+# Update the layout of the figure
+fig.update_layout(
+    scene=dict(
+        xaxis=dict(
+            title="x - axis",
+            backgroundcolor="white",
+            gridcolor="lightgrey",
+            showbackground=True,
+            zerolinecolor="white",
+        ),
+        yaxis=dict(
+            title="y - axis",
+            backgroundcolor="white",
+            gridcolor="lightgrey",
+            showbackground=True,
+            zerolinecolor="white",
+        ),
+        zaxis=dict(
+            title="z - axis",
+            backgroundcolor="white",
+            gridcolor="lightgrey",
+            showbackground=True,
+            zerolinecolor="white",
+        ),
+    ),
+    legend=dict(
+        yanchor="top",
+        y=0.99,
+        xanchor="left",
+        x=0.01,
+        bgcolor="rgba(255, 255, 255, 0.7)",
+    ),
+    title=dict(text="Theta* vs A*"),
+    scene_camera=camera,
+)
+
+# Save the figure as a html file
+fig.write_html("theta_star.html")
+# Show the figure in a new tab
+fig.show()
